b75ddbd5736ed6229f2d1604b0e56cd38115be94
Matrex7 <80312529+Matrex7@users.noreply.github.com>
2021-03-21 16:19:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,7 @@
 const Discord = require('discord.js')
-const client = new Discord.Client();
+const matrex = new Discord.Client();
 const {owner,token,prefix} = require('./config.json')
-client.on('ready',()=>{
-    console.log('ready')
-    const activities_list = [
-        " Welcome to MOREX STORE", 
-        "/-help",
-        "open ticket to buy anything", 
-        "dont ping owner"
-        ];
-        setInterval(() => {
-            const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); 
-            client.user.setActivity(activities_list[index]); 
-        }, 10000);
-})
-client.on('message',message =>{
+matrex.on('message',message =>{
     if(message.author.bot || message.channel.type === 'dm') return;
     const sugchan = message.guild.channels.cache.get('822808062649696287')
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
@@ -27,4 +14,4 @@ client.on('message',message =>{
     const sugEmbed = new Discord.MessageEmbed().setColor("RANDOM").setFooter(`Welcome`).setTimestamp().setTitle(`Maded by ${message.author.tag}`).setDescription(args.join(" "))
     sugchan.send(sugEmbed)
 }})
-client.login(token)
+morex.login(token)
