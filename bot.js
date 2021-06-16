285edeab6591bfb118491a244c272834f5782ddf
Tanya <80748971+Tanya575@users.noreply.github.com>
2021-06-16 12:21:37 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,21 @@
 const Discord = require('discord.js')
 const matrex = new Discord.Client();
-const {owner,token,prefix} = require('./config.json')
-matrex.on('message',message =>{
-    if(message.author.bot || message.channel.type === 'dm') return;
+const {owner, token, prefix} = require('./config.json')
+matrex.on('message', message =>{
+    if(message.author.bot || message.channel.type === 'dm' || !message.content.startsWith(prefix)) return;
     const sugchan = message.guild.channels.cache.get('822808062649696287')
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
+      
+    if (!cmd) return
     if(cmd === `suggest`) { 
      if(!args.length){
         message.reply('im not dump to send empty suggestion')
          return;
     }
-    const sugEmbed = new Discord.MessageEmbed().setColor("RANDOM").setFooter(`Welcome`).setTimestamp().setTitle(`Maded by ${message.author.tag}`).setDescription(args.join(" "))
+    const sugEmbed = new Discord.MessageEmbed() .setColor("RANDOM") .setFooter(`Welcome`) .setTimestamp() .setTitle(`Maded by ${message.author.tag}`) .setDescription(args.join(" "))
     sugchan.send(sugEmbed)
+        
+    message.delete();
 }})
 morex.login(token)
